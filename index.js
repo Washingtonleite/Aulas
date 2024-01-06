f8f84bb328273898483c1b946b55928e1b81e416
Washingtonleite <washingtonleitte@gmail.com>
2024-01-06 02:08:59 -0300

    desafio-logica-inicial

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-let nome = "Washington"
-let XP = 8000
-let nivel = true
+let nome = "Raio"
+let xp = 7000
+let nivel = ""
 
-if (XP < 1000){
-    console.log("Ferro")
-}if (XP > 1000){
-    console,log("Bronze")
-}else if (XP <= 2000){
-    console.log("Bronze")
-}else if (XP > 2000){
-    console.log("Prata")
-}else if (XP <= 5000){
-    console.log("Prata")
-}else if (XP > 5000){
-    console.log("Ouro")
-}else if (XP <= 7000){
-    console.log ("outro")
-}else if (XP > 7000){
-    console.log("Platina")
-}else if (XP <= 8000){
-    console.log("Platina")
-}else if (XP > 8000){
-    console.log("Ascendente")
-}else if (XP <= 9000){
-    console.log("Ascendente")
-}else if (XP > 9000){
-    console.log("Imortal")
-}else if (XP <= 10000){
-    console.log("Imortal")
-}else if (XP >= 10001){
-    console.log("Radiante")
+if (xp < 1000) {
+    nivel = "Ferro"
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze"
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata"
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro"
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina"
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente"
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
 }
 
-console.log("O Herói de nome " + nome)
-console.log("está no nível de " + XP)
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
